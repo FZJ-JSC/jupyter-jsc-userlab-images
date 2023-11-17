b976f63cc387c370290e1094bf9a01eaa6e295fd
Alice Grosch <a.grosch@fz-juelich.de>
2023-11-17 18:15:51 +0100

    Add jupyterlab start command

--- lmod/files/load_modules.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source /tmp/custom/load_jupyter_version.sh
-source /tmp/custom/load_jupyter_modules.sh
---
+++ lmod/files/start_jupyter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /tmp/custom/load_jupyter_version.sh
+source /tmp/custom/load_jupyter_modules.sh
+
+timeout 30d jupyterhub-singleuser
